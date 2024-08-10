88c97c3928a4fe8d8063a591fdc393ebb22df9bb
adnan1175 <138349081+adnan1175@users.noreply.github.com>
2024-08-10 19:17:16 +0600

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-/etc/NX/nxserver --startup
+groupadd -r $USER -g 433 \                                     && useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
+&& adduser $USER sudo \
+&& mkdir /home/$USER \                                         && chown -R $USER:$USER /home/$USER \
+&& echo $USER':'$PASSWORD | chpasswd                           /etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
